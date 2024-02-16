15844295996f1dad5053e4a5fff35091354b8e9e
Luiz Francisco <141175753+Luizzz2505@users.noreply.github.com>
2024-02-16 17:57:30 -0300

    Add files via upload

---
+++ ws-eclipse/curso_programacao/src/curso_programacao/Aula003.java
@@ -0,0 +1,27 @@
+package curso_programacao;
+
+public class Aula003 {
+
+	public static void main(String[] args) {
+		
+		int x, y;
+		
+		x = 5;
+		
+		y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		int b = 6;
+		int B = 8;
+		int h = 5;
+		double area;
+		
+		area = (b + B) / 2 * h;
+		
+		System.out.println(area);
+
+	}
+
+}
